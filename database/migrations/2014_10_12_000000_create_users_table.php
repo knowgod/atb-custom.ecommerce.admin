a202b25de2f52e8c0100d95a700db1af77b1e572
Seva Yatsyuk <yatsyuk.v@gmail.com>
2015-12-29 13:50:33 +0200

    Changes to User model structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('password', 100);
+
+            $table->string('user_group', 55);
+            $table->string('register_source', 55);
+
             $table->rememberToken();
             $table->timestamps();
         });
