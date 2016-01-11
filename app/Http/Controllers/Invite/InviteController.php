1f3e3d57ed0835b753610a111f3f6911e76e857e
akonstantin <konstantin.alekseyev@insitedigital.co.uk>
2016-01-11 18:56:13 +0200

    WEB-3730. Invitations functionality update

--- app/Http/Controllers/Invite/InviteController.php
+++ app/Http/Controllers/Invite/InviteController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Invite;
 
+use App\Models\Invitations\Repositories\InviteRepository;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
@@ -11,6 +11,19 @@ use Illuminate\Support\Facades\Redirect;
 
 class InviteController extends Controller
 {
+    public $inviteRepo = null;
+    protected $_itemsPerPage = 10;
+
+    public function __construct(InviteRepository $inviteRepository){
+        $this->inviteRepo = $inviteRepository;
+    }
+
+    public function index()
+    {
+        $invitations = $this->inviteRepo->getPaginatedInvitations($this->_itemsPerPage);
+        return view('invite.list', array('collection' => $invitations));
+    }
+
     public function create()
     {
         return view('invite.form');
@@ -20,10 +33,19 @@ class InviteController extends Controller
     {
         Mail::send('invite.email', ['email' => $request->email], function ($message) use ($request) {
             $message->from($request->user()->email, 'NuMe');
-
             $message->to($request->email, 'name')->subject('Invitation!');
         });
 
         return Redirect::route('invite')->with('message', 'Invitation has been sent successfully!');
+    }
+
+    public function resend(Request $request)
+    {
+        var_dump($request);die;
+    }
+
+    public function massResend()
+    {
+
     }
 }
