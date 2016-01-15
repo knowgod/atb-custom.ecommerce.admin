4d04c24aa9fe38f1be586be698fb648e4211ef9b
Seva Yatsyuk <yatsyuk.v@gmail.com>
2016-01-15 18:39:52 +0200

    [WEB-3712] Migrating user Entity to Doctrine

---
+++ app/Models/Users/Repositories/DUserRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Users\Repositories;
+
+use App\Models\Users\Entities\DUser as User;
+
+/**
+ * @category Atypicalbrands
+ * Written by: vyatsyuk@atypicalbrands.com
+ * Date: 05.01.16
+ *
+ */
+class DUserRepository {
+
+}
+
