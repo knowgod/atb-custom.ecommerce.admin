0dc6dee952b44bd0edd609dfe4cd5830675a5b07
Seva Yatsyuk <yatsyuk.v@gmail.com>
2016-01-19 17:28:24 +0200

    [WEB-3714] add changes to migration

--- database/migrations/doctrine/Version20160118150117.php
+++ database/migrations/doctrine/Version20160118150117.php
@@ -13,12 +13,8 @@ class Version20160118150117 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('DROP INDEX password_resets_email_index ON password_resets');
-        $this->addSql('DROP INDEX password_resets_token_index ON password_resets');
         $this->addSql('ALTER TABLE password_resets ADD PRIMARY KEY (email)');
         $this->addSql('ALTER TABLE users DROP user_group, DROP permission_group_id, CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE firstname firstname VARCHAR(255) DEFAULT NULL, CHANGE lastname lastname VARCHAR(255) DEFAULT NULL, CHANGE email email VARCHAR(32) NOT NULL, CHANGE password password VARCHAR(255) NOT NULL, CHANGE register_source register_source VARCHAR(255) DEFAULT NULL, CHANGE remember_token remember_token VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE users RENAME INDEX users_email_unique TO UNIQ_1483A5E9E7927C74');
     }
 
     /**
@@ -27,10 +23,6 @@ class Version20160118150117 extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE password_resets DROP INDEX primary, ADD INDEX password_resets_email_index (email)');
-        $this->addSql('CREATE INDEX password_resets_token_index ON password_resets (token)');
         $this->addSql('ALTER TABLE users ADD user_group VARCHAR(100) NOT NULL COLLATE utf8_unicode_ci, ADD permission_group_id BIGINT NOT NULL, CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL, CHANGE firstname firstname VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE lastname lastname VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE email email VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE register_source register_source VARCHAR(100) NOT NULL COLLATE utf8_unicode_ci, CHANGE password password VARCHAR(100) NOT NULL COLLATE utf8_unicode_ci, CHANGE remember_token remember_token VARCHAR(100) DEFAULT NULL COLLATE utf8_unicode_ci');
-        $this->addSql('ALTER TABLE users RENAME INDEX uniq_1483a5e9e7927c74 TO users_email_unique');
     }
 }
