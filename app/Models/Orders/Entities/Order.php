1e4e24d4c0d5def7dae84491e5b20f0a1e38fd7a
Seva Yatsyuk <yatsyuk.v@gmail.com>
2016-01-19 15:49:15 +0200

    [WEB-3714] finalize order entity, add seeder

--- app/Models/Orders/Entities/Order.php
+++ app/Models/Orders/Entities/Order.php
@@ -45,28 +45,28 @@ class Order extends DoctrineModel{
     protected $email;
 
     /**
-     * @ORM\Column(type="decimal", length=12, precision=4, nullable=true)
+     * @ORM\Column(type="decimal", precision=12, scale=4, nullable=true)
      */
 
     protected $grand_total;
 
     /**
-     * @ORM\Column(type="decimal", length=12, precision=4, nullable=true)
+     * @ORM\Column(type="decimal", precision=12, scale=4, nullable=true)
      */
 
     protected $total_paid;
+
+    /**
+     * @ORM\Column(type="decimal", precision=12, scale=4, nullable=true)
+     */
+
+    protected $discount_amount;
 
     /**
      * @ORM\Column(type="string", length=55, nullable=true)
      */
 
     protected $payment_method;
-
-    /**
-     * @ORM\Column(type="decimal", length=12, precision=4, nullable=true)
-     */
-
-    protected $discount_amount;
 
     /**
      * @ORM\Column(type="string", length=32, nullable=true)
@@ -85,5 +85,181 @@ class Order extends DoctrineModel{
 
 
     protected $hidden = [];
+
+    /**
+     * @return mixed
+     */
+    public function getIncrementId(){
+        return $this->increment_id;
+    }
+
+    /**
+     * @param mixed $increment_id
+     * @return Order
+     */
+    public function setIncrementId($increment_id){
+        $this->increment_id = $increment_id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus(){
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     * @return Order
+     */
+    public function setStatus($status){
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerName(){
+        return $this->customer_name;
+    }
+
+    /**
+     * @param mixed $customer_name
+     * @return Order
+     */
+    public function setCustomerName($customer_name){
+        $this->customer_name = $customer_name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail(){
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     * @return Order
+     */
+    public function setEmail($email){
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGrandTotal(){
+        return $this->grand_total;
+    }
+
+    /**
+     * @param mixed $grand_total
+     * @return Order
+     */
+    public function setGrandTotal($grand_total){
+        $this->grand_total = $grand_total;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTotalPaid(){
+        return $this->total_paid;
+    }
+
+    /**
+     * @param mixed $total_paid
+     * @return Order
+     */
+    public function setTotalPaid($total_paid){
+        $this->total_paid = $total_paid;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPaymentMethod(){
+        return $this->payment_method;
+    }
+
+    /**
+     * @param mixed $payment_method
+     * @return Order
+     */
+    public function setPaymentMethod($payment_method){
+        $this->payment_method = $payment_method;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDiscountAmount(){
+        return $this->discount_amount;
+    }
+
+    /**
+     * @param mixed $discount_amount
+     * @return Order
+     */
+    public function setDiscountAmount($discount_amount){
+        $this->discount_amount = $discount_amount;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShippingCountryCode(){
+        return $this->shipping_country_code;
+    }
+
+    /**
+     * @param mixed $shipping_country_code
+     * @return Order
+     */
+    public function setShippingCountryCode($shipping_country_code){
+        $this->shipping_country_code = $shipping_country_code;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCouponCode(){
+        return $this->coupon_code;
+    }
+
+    /**
+     * @param mixed $coupon_code
+     * @return Order
+     */
+    public function setCouponCode($coupon_code){
+        $this->coupon_code = $coupon_code;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQty(){
+        return $this->qty;
+    }
+
+    /**
+     * @param mixed $qty
+     * @return Order
+     */
+    public function setQty($qty){
+        $this->qty = $qty;
+        return $this;
+    }
 
 }
---
+++ database/seeds/OrdersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Orders\Entities\Order;
+
+class OrdersTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+
+        $faker = Faker\Factory::create();
+        $numPrefix = $faker->randomDigit;
+
+        for($i = 0; $i <= 50; $i++){
+            $order = new Order();
+            $grandTotal = $faker->randomFloat(null, 10.99, 99.99); // 48.8932
+            $discount = $grandTotal * 0.2;
+
+            $order->setEmail($faker->email)
+                    ->setIncrementId($numPrefix . '0000' . $i)
+                    ->setCustomerName($faker->firstName . ' ' . $faker->lastName)
+                    ->setQty($faker->numberBetween(0, 10))
+                    ->setStatus($faker->randomElement(['Under Review', 'Pending', 'Shipped', 'Closed']))
+                    ->setGrandTotal($grandTotal)
+                    ->setDiscountAmount($discount)
+                    ->setTotalPaid($grandTotal)
+                    ->setShippingCountryCode($faker->countryCode)
+                    ->setPaymentMethod($faker->randomElement(['Credit Card', 'PayPal']))
+                    ->setCouponCode('NUME' . $faker->randomElement(['HAIR', 'AWSM', 'DISC']));
+            $order->save();
+        }
+
+    }
+}
