e26f374c5b802dc8f1de742534eb8f6af7ed49bb
Seva Yatsyuk <yatsyuk.v@gmail.com>
2016-02-12 17:24:40 +0200

    [WEB-3957] cleanup obsolete files and comments

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,16 +9,6 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //$this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return Response
