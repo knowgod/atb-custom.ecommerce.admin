d6f1bbb14482ef6d6ad3109727480cc472ede756
Seva Yatsyuk <yatsyuk.v@gmail.com>
2016-02-24 19:16:21 +0200

    [WEB-3812] removed seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,6 @@ class DatabaseSeeder extends Seeder {
      */
     public function run(){
         $this->call(UsersTableSeeder::class);
-        $this->call(OrdersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
     }
 }
