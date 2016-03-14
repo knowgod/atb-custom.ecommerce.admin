a98458b8680b8b6f6b22b244e7faa0c5874bff21
Seva Yatsyuk <yatsyuk.v@gmail.com>
2016-03-14 16:54:34 +0200

    [WEB-3957] refactoring of doctrine model and google auth controller

--- app/Contracts/DoctrineModel.php
+++ app/Contracts/DoctrineModel.php
@@ -6,17 +6,23 @@
  * Date: 18.01.16
  *
  */
+
 namespace App\Contracts;
 
-use LaravelDoctrine\ORM\Facades\EntityManager;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\PersistentCollection;
 use LaravelDoctrine\ORM\Serializers;
 use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Contracts\Support\Arrayable;
 use JsonSerializable;
 
-
-
 abstract class DoctrineModel implements Jsonable, JsonSerializable, Arrayable {
+
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     *
+     */
 
     protected $_em;
 
@@ -28,18 +34,27 @@ abstract class DoctrineModel implements Jsonable, JsonSerializable, Arrayable {
 
     protected $hidden = [];
 
+    public function __construct(EntityManagerInterface $em){
+        $this->_em = $em;
+        $this->_init();
+    }
+
 
     public function save(){
-
-        EntityManager::persist($this);
-        EntityManager::flush();
+        $this->getEntityManager()->persist($this);
+        $this->getEntityManager()->flush();
         return $this;
     }
 
     public function remove(){
-        EntityManager::remove($this);
-        EntityManager::flush();
+        $this->getEntityManager()->remove($this);
+        $this->getEntityManager()->flush();
         return;
+    }
+
+    public function fillFromArray(array $attributes = []){
+        $this->fill($attributes);
+        return $this;
     }
 
     /**
@@ -71,19 +86,74 @@ abstract class DoctrineModel implements Jsonable, JsonSerializable, Arrayable {
      */
     public function toArray(){
         $objectProperties = get_object_vars($this);
-        foreach($this->getHiddenFields() as $hiddenFieldName){
-            if(isset($objectProperties[$hiddenFieldName]))
+        foreach ($this->getHiddenFields() as $hiddenFieldName){
+            if (isset($objectProperties[$hiddenFieldName]))
                 unset($objectProperties[$hiddenFieldName]);
         }
-        return $objectProperties;
+        return array_merge($objectProperties, $this->relationsToArray());
     }
 
-    protected function getEntityManager(){
-        return $this->_em;
+    /**
+     * Get the model's relationships in array form.
+     *
+     * @return array
+     */
+    public function relationsToArray(){
+        $attributes = [];
+        $objectProperties = get_object_vars($this);
+
+        foreach ($this->getHiddenFields() as $hiddenFieldName){
+            if (isset($objectProperties[$hiddenFieldName]))
+                unset($objectProperties[$hiddenFieldName]);
+        }
+
+        foreach ($objectProperties as $key => $value){
+            if ($value instanceof Arrayable || $value instanceof PersistentCollection){
+                $relation = $value->toArray();
+                $attributes[$key] = $relation;
+            }
+            if (isset($relation)){
+                unset($relation);
+            }
+        }
+
+        return $attributes;
     }
+
+    /**
+     * @return EntityManagerInterface
+     *
+     */
+
+    public function getEntityManager(){
+        return ($this->_em) ? $this->_em : app('em');
+    }
+
+    /**
+     * @return array
+     */
 
     protected function getHiddenFields(){
-        return $this->hidden;
+        $propsHiddenByDefault = ['hidden', '_em'];
+        return array_merge($this->hidden, $propsHiddenByDefault);
     }
 
+    protected function _init(){
+        return;
+    }
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository
+     */
+    public function getRepository(){
+        return $this->getEntityManager()->getRepository(get_class($this));
+    }
+
+    public function fill(array $attributes){
+        foreach ($attributes as $key => $value){
+            if (property_exists($this, $key)){
+                $this->{$key} = $value;
+            }
+        }
+    }
 }
